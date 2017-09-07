c389cf8e059fd607fadd8f0055c3464bf1c55047
X1Warrior <zsto7638@gmail.com>
2017-09-07 12:43:16 -0600

    Practiced some HelloWorld sayings

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -2,5 +2,9 @@ package process.controller;
 
 public class ProcessController
 {
-
+	public void start()
+	{
+		System.out.println("i am the master of this app fear me.");
+		System.out.println("dont try me son!");
+	}
 }
--- src/process/model/ProccessRunner.java
+++ src/process/model/ProccessRunner.java
@@ -1,6 +1,12 @@
 package process.model;
 
+import process.controller.ProcessController;
+
 public class ProccessRunner
 {
-
+	public static void main(String [] args)
+	{
+		ProcessController appController = new ProcessController();
+		appController.start();
+	}
 }
