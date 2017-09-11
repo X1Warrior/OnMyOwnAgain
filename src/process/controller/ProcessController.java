b6d71319fa1f460c2895c761dfbce8e799c755c8
X1Warrior <zsto7638@gmail.com>
2017-09-11 12:14:37 -0600

    Made a Play Doh Circle and printed it.

--- src/process/controller/ProcessController.java
+++ src/process/controller/ProcessController.java
@@ -1,10 +1,25 @@
 package process.controller;
 
+import process.model.PlayDohCircle;
+/**
+ * 
+ * @author Zach Stout
+ *
+ */
 public class ProcessController
 {
 	public void start()
 	{
 		System.out.println("i am the master of this app fear me.");
 		System.out.println("dont try me son!");
+	
+	PlayDohCircle firstCircle = new PlayDohCircle();
+	PlayDohCircle secondCircle;
+	secondCircle = new PlayDohCircle();
+	
+	System.out.println("Look I made a circle");
+	System.out.println(firstCircle);
+	System.out.println(secondCircle);
 	}
+	
 }
