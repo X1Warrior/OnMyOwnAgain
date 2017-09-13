0bfaf7123ed396527543879f7badb202dbf26527
X1Warrior <zsto7638@gmail.com>
2017-09-13 13:43:38 -0600

    aded a setter for the size data member.

--- src/process/model/PlayDohCircle.java
+++ src/process/model/PlayDohCircle.java
@@ -11,14 +11,21 @@ public class PlayDohCircle
 	this.size = 5;
 	
 	}
-public PlayDohCircle(int size) 
-{
+	
+	public PlayDohCircle(int size) 
+	{
 	this.size = size;
-}
-public String toString()
-{
+	}
+
+	public void setSize(int newSize)
+	{
+	this.size = newSize;
+	}
+
+
+	public String toString()
+	{
 	String description = "I am a circle, my size is" + size;
 	return description;
-}
-
+	}
 }
